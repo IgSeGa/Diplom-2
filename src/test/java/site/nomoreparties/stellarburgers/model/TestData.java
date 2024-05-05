e87f2b9f96096b4dfd4247a6141d0f95a6e2078e
igavrilin <igavrilin@rbc.ru>
2024-05-05 20:26:53 +0300

    Diplom-2

---
+++ src/test/java/site/nomoreparties/stellarburgers/model/TestData.java
@@ -0,0 +1,12 @@
+package site.nomoreparties.stellarburgers.model;
+
+public interface TestData {
+    String TESTMAIL = "diplomauser@praktikum.ru";
+    String TESTPASS = "123456";
+    String TESTNAME = "Vasya";
+    String SECONDMAIL = "diplomauser1@praktikum.ru";
+    String THIRDMAIL = "diplomauser2@praktikum.ru";
+    String SECONDNAME = "Vasyan";
+    String THIRDNAME = "Vasyanishe";
+    String [] INGREDS = {"61c0c5a71d1f82001bdaaa70", "61c0c5a71d1f82001bdaaa71"};
+}
